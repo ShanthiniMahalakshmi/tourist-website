55ef8dc558c2675e6d4793927f289e18c2421d6a
ShanthiniMahalakshmi <shanthinimahalakshmi18@gmail.com>
2021-04-21 13:53:43 +0530

    Final commit

--- src/Components/Frames/Home.js
+++ src/Components/Frames/Home.js
@@ -2,13 +2,12 @@ import React from "react";
 import "../../App.css";
 import Cards from "../Cards";
 import MainSection from "../MainSection";
-import Footer from "../Footer";
+
 function Home() {
   return (
     <>
       <MainSection />
       <Cards />
-      <Footer />
     </>
   );
 }
